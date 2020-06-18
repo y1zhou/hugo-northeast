58a4a256ebffe74564a4517ed78cd76d3f12c59c
y1zhou <zhou.zy.yi@gmail.com>
2020-06-18 18:10:50 -0400

    feat(font): add custom fonts

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,15 +1,29 @@
 const themeDir = __dirname + "/../../";
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-    purge: [
-        themeDir + "layouts/**/*.html",
-        themeDir + "content/**/*.html",
-        "layouts/**/*.html",
-        "content/**/*.html",
-        "exampleSite/layouts/**/*.html",
-        "exampleSite/content/**/*.html",
-    ],
+    purge: {
+        enabled: true,
+        content: [
+            themeDir + "layouts/**/*.html",
+            themeDir + "content/**/*.html",
+            "layouts/**/*.html",
+            "content/**/*.html",
+            "exampleSite/layouts/**/*.html",
+            "exampleSite/content/**/*.html",
+        ],
+    },
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans],
+                serif: ['"PT Serif"', ...defaultTheme.fontFamily.serif],
+                mono: ['"JetBrains Mono"', ...defaultTheme.fontFamily.mono],
+            },
+            screens: {
+                dark: { raw: "(prefers-color-scheme: dark)" },
+                // => @media (prefers-color-scheme: dark) { ... }
+            },
+        },
     },
     variants: {},
     plugins: [],
