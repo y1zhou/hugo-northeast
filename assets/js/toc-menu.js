88dc53bcf4d2ad9436e1df31ae75ce8f756e5e3a
y1zhou <zhou.zy.yi@gmail.com>
2020-06-25 14:15:13 -0400

    fix(button): move TOC to the right side

--- assets/js/toc-menu.js
+++ assets/js/toc-menu.js
@@ -1,21 +1,24 @@
-const TOCLinks = document.querySelectorAll("#toc-list a");
-TOCLinks.forEach((element) => {
-    element.addEventListener("click", toggleTOC);
-});
-
 function toggleTOC() {
     let tocMask = document.getElementById("toc-background-mask");
     let tocMenu = document.getElementById("toc-container");
 
-    if (tocMask.classList.contains("invisible")) {
-        tocMask.classList.remove("opacity-0", "invisible");
-        tocMask.classList.add("opacity-75", "visible");
-        tocMenu.classList.remove("opacity-0", "invisible");
-        tocMenu.classList.add("opacity-100", "visible");
+    if (tocMask.classList.contains("bg-opacity-0")) {
+        tocMask.classList.remove("bg-opacity-0", "invisible");
+        tocMask.classList.add("bg-opacity-75", "visible");
+        tocMenu.classList.remove("opacity-0", "translate-x-full");
+        tocMenu.classList.add("opacity-100");
     } else {
-        tocMask.classList.remove("opacity-75", "visible");
-        tocMask.classList.add("opacity-0", "invisible");
-        tocMenu.classList.remove("opacity-100", "visible");
-        tocMenu.classList.add("opacity-0", "invisible");
+        tocMenu.classList.remove("opacity-100");
+        tocMenu.classList.add("opacity-0", "translate-x-full");
+        tocMask.classList.remove("bg-opacity-75", "visible");
+        tocMask.classList.add("bg-opacity-0", "invisible");
     }
 }
+document.addEventListener("DOMContentLoaded", function () {
+    const TOCLinks = document.querySelectorAll("#toc-list a");
+    TOCLinks.forEach((element) => {
+        element.addEventListener("click", () => {
+            setTimeout(toggleTOC, 0);
+        });
+    });
+});
