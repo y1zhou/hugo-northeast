c15d2c4cc8f0851ce0b9da8702d9b2c5df9375d2
y1zhou <zhou.zy.yi@gmail.com>
2020-06-25 16:46:36 -0400

    perf(toc): avoid getting element every time the buttons are clicked

--- assets/js/toc-menu.js
+++ assets/js/toc-menu.js
@@ -1,24 +1,26 @@
-function toggleTOC() {
-    let tocMask = document.getElementById("toc-background-mask");
-    let tocMenu = document.getElementById("toc-container");
-
-    if (tocMask.classList.contains("bg-opacity-0")) {
-        tocMask.classList.remove("bg-opacity-0", "invisible");
-        tocMask.classList.add("bg-opacity-75", "visible");
-        tocMenu.classList.remove("opacity-0", "translate-x-full");
-        tocMenu.classList.add("opacity-100");
-    } else {
-        tocMenu.classList.remove("opacity-100");
-        tocMenu.classList.add("opacity-0", "translate-x-full");
-        tocMask.classList.remove("bg-opacity-75", "visible");
-        tocMask.classList.add("bg-opacity-0", "invisible");
-    }
-}
 document.addEventListener("DOMContentLoaded", function () {
-    const TOCLinks = document.querySelectorAll("#toc-list a");
+    const tocMask = document.getElementById("toc-background-mask");
+    const tocMenu = document.getElementById("toc-container");
+    const tocButton = document.getElementById("toc-button");
+    const TOCLinks = document.querySelectorAll(
+        "#toc-list a, #toc-background-mask, #toc-container, #toc-button"
+    );
+    TOCLinks.add;
+
     TOCLinks.forEach((element) => {
-        element.addEventListener("click", () => {
-            setTimeout(toggleTOC, 0);
+        element.addEventListener("click", (e) => {
+            e.stopPropagation();
+            if (tocMask.classList.contains("bg-opacity-0")) {
+                tocMask.classList.add("bg-opacity-75", "visible");
+                tocMask.classList.remove("bg-opacity-0", "invisible");
+                tocMenu.classList.add("opacity-100");
+                tocMenu.classList.remove("opacity-0", "translate-x-full");
+            } else {
+                tocMenu.classList.add("opacity-0", "translate-x-full");
+                tocMenu.classList.remove("opacity-100");
+                tocMask.classList.add("bg-opacity-0", "invisible");
+                tocMask.classList.remove("bg-opacity-75", "visible");
+            }
         });
     });
 });
