cd6c96ea86b6eee79d9452a65900a8a01ac71d19
y1zhou <zhou.zy.yi@gmail.com>
2020-06-25 23:58:24 -0400

    perf(toc): reworked animations for TOC; preload site CSS in head

--- assets/js/toc-menu.js
+++ assets/js/toc-menu.js
@@ -1,26 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
     const tocMask = document.getElementById("toc-background-mask");
     const tocMenu = document.getElementById("toc-container");
-    const tocButton = document.getElementById("toc-button");
-    const TOCLinks = document.querySelectorAll(
-        "#toc-list a, #toc-background-mask, #toc-container, #toc-button"
+    const ToggleTOC = document.querySelectorAll(
+        "#toc-button, #toc-background-mask, #toc-list a"
     );
-    TOCLinks.add;
 
-    TOCLinks.forEach((element) => {
+    ToggleTOC.forEach((element) => {
         element.addEventListener("click", (e) => {
             e.stopPropagation();
-            if (tocMask.classList.contains("bg-opacity-0")) {
-                tocMask.classList.add("bg-opacity-75", "visible");
-                tocMask.classList.remove("bg-opacity-0", "invisible");
-                tocMenu.classList.add("opacity-100");
-                tocMenu.classList.remove("opacity-0", "translate-x-full");
+            if (tocMask.classList.contains("active")) {
+                tocMenu.classList.add("translate-x-full");
+                setTimeout(function () {
+                    tocMask.style.visibility = "";
+                }, 150);
             } else {
-                tocMenu.classList.add("opacity-0", "translate-x-full");
-                tocMenu.classList.remove("opacity-100");
-                tocMask.classList.add("bg-opacity-0", "invisible");
-                tocMask.classList.remove("bg-opacity-75", "visible");
+                tocMask.style.visibility = "visible";
+                tocMenu.classList.remove("translate-x-full");
             }
+            tocMask.classList.toggle("active");
         });
     });
 });
