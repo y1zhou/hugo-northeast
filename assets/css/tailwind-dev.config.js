82f767d1df2bf165c1802bca65b7cb5fa238e3e0
y1zhou <zhou.zy.yi@gmail.com>
2020-11-20 22:43:29 -0500

    fix: config changes as suggested by the official upgrade guide
    The [guide](https://tailwindcss.com/docs/upgrading-to-v2) also suggested color and font configs, but
    I'm skipping these because the new defaults seem pretty nice overall

--- assets/css/tailwind-dev.config.js
+++ assets/css/tailwind-dev.config.js
@@ -1,17 +1,13 @@
 const themeDir = __dirname + "/../../";
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-    important: true,
+    darkMode: "media",
     theme: {
         extend: {
             fontFamily: {
                 sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans],
                 serif: ['"PT Serif"', ...defaultTheme.fontFamily.serif],
                 mono: ['"JetBrains Mono"', ...defaultTheme.fontFamily.mono],
-            },
-            screens: {
-                dark: { raw: "(prefers-color-scheme: dark)" },
-                // => @media (prefers-color-scheme: dark) { ... }
             },
         },
     },
--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,11 +1,6 @@
 const themeDir = __dirname + "/../../";
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-    future: {
-        removeDeprecatedGapUtilities: true,
-        purgeLayersByDefault: true,
-    },
-    important: true,
     purge: {
         enabled: true,
         content: [
@@ -17,16 +12,13 @@ module.exports = {
             "exampleSite/content/**/*.html",
         ],
     },
+    darkMode: "media",
     theme: {
         extend: {
             fontFamily: {
                 sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans],
                 serif: ['"PT Serif"', ...defaultTheme.fontFamily.serif],
                 mono: ['"JetBrains Mono"', ...defaultTheme.fontFamily.mono],
-            },
-            screens: {
-                dark: { raw: "(prefers-color-scheme: dark)" },
-                // => @media (prefers-color-scheme: dark) { ... }
             },
         },
     },
