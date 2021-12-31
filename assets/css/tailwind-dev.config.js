bb1538a63d9c86852612f2ff932e98ced0d82de5
y1zhou <zhou.zy.yi@gmail.com>
2021-12-31 17:29:58 -0500

    feat(dep): tailwind version bump

--- assets/css/tailwind-dev.config.js
+++ assets/css/tailwind-dev.config.js
@@ -13,6 +13,5 @@ module.exports = {
             },
         },
     },
-    variants: {},
     plugins: [],
 };
--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -2,17 +2,14 @@ const themeDir = __dirname + "/../../";
 const defaultTheme = require("tailwindcss/defaultTheme");
 const neColors = require("./tailwind-colors"); // ne stands for northeast;
 module.exports = {
-    purge: {
-        enabled: true,
-        content: [
-            themeDir + "layouts/**/*.html",
-            themeDir + "content/**/*.html",
-            "layouts/**/*.html",
-            "content/**/*.html",
-            "exampleSite/layouts/**/*.html",
-            "exampleSite/content/**/*.html",
-        ],
-    },
+    content: [
+        themeDir + "layouts/**/*.html",
+        themeDir + "content/**/*.html",
+        "layouts/**/*.html",
+        "content/**/*.html",
+        "exampleSite/layouts/**/*.html",
+        "exampleSite/content/**/*.html",
+    ],
     theme: {
         extend: {
             fontFamily: {
@@ -25,6 +22,5 @@ module.exports = {
             },
         },
     },
-    variants: {},
     plugins: [],
 };
